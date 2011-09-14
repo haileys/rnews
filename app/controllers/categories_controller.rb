091b3307c7fac96cc573bf9545eeb0e4638eb34c
charliesome <charlie@charliesomerville.com>
2011-09-14 17:37:32 +1000

    find_by_name! in categories controller to show 404 (RecordNotFound) instead of 500 (NoMethodError on nil method call)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
     @category_views = Category::VIEWS.map { |v| [v, Category.send(v)] }
   end
   def show
-    @category = Category.find_by_name params[:id]
+    @category = Category.find_by_name! params[:id]
     @views = @category.views
     @view = @views.include?(params[:view]) ? params[:view] : @views.first
     @stories = @category.send @view
