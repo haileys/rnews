fd72e7d0786d2cf41b4ea28aa4982226ef8318d5
charliesome <charlie@charliesomerville.com>
2011-09-17 17:18:55 +1000

    fixed

--- check_deploy.rb
+++ check_deploy.rb
@@ -12,10 +12,15 @@ if status["last_build_status"] == 0
   
   puts "Deploying to #{build["commit"]}... "
   
-  system("rake test >/dev/null &")
   output = `git fetch && git merge #{build["commit"]}`
   
   unless output.include? "Already up-to-date"
+    fork do
+      $stdout = File.open("/dev/null", "w")
+      system("rake test")
+      exit!
+    end
+    
     system("touch tmp/restart.txt")
     puts "Restarted app"
   end
