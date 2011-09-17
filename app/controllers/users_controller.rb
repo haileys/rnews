2848bdd7bc3f2d84893365a8196309494c737222
charliesome <charlie@charliesomerville.com>
2011-09-17 16:39:30 +1000

    make UsersController raise a RecordNotFound error when it can't find the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find_by_username params[:id]
+    @user = User.find_by_username! params[:id]
   end
   
   def create
