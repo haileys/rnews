a026890c29cfb80922d244d7ac39073f22080ce4
charliesome <charlie@charliesomerville.com>
2011-09-17 16:40:04 +1000

    tests for UsersController and SessionsController

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get login page" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should create session" do
+    post :create, { :username => "charlie", :password => "foo" }
+    assert_equal users(:charlie), @controller.current_user
+    assert_equal users(:charlie).id, session[:user_id]
+    assert_redirected_to "/"
+  end
+  
+  test "should not create session" do
+    post :create, { :username => "charlie", :password => "bar" }
+    assert_nil session[:user_id]
+    assert_select ".attn.bad"
+  end
+  
+  test "should logout" do
+    get :destroy, {}, { :uid => users(:charlie) }
+    assert_nil @controller.current_user
+    assert_nil session[:user_id]
+    assert_redirected_to "/"
+  end
+  
+  test "should logout and return to url" do
+    get :destroy, {}, { :uid => users(:charlie), :return_url => stories(:one) }
+    assert_redirected_to stories(:one)
+  end
+  
+  test "should login and return to url" do
+    post :create, { :username => "charlie", :password => "foo" }, { :return_url => stories(:one) }
+    assert_redirected_to stories(:one)
+  end
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should show register form" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should show errors and not create user" do
+    assert_no_difference "User.count" do
+      post :create, { :user => { } }
+    end
+    assert_select ".attn.bad"
+    assert_select ".field_with_errors" do
+      assert_select "#user_username"
+      assert_select "#user_password"
+    end
+  end
+  
+  test "should create user" do
+    assert_difference "User.count" do
+      post :create, { :user => { :username => "zzz", :password => "foobar", :password_confirmation => "foobar", :bio => "test bio" } }
+    end
+    assert_redirected_to "/"
+    assert_not_equal nil, session[:user_id]
+    user = @controller.current_user.reload
+    assert_equal "zzz", user.username
+    assert_equal "test bio", user.bio
+    assert user.authenticate("foobar")
+  end
+  
+  test "should create user and return to url" do
+    assert_difference "User.count" do
+      post :create, { :user => { :username => "zzz123", :password => "foobar", :password_confirmation => "foobar", :bio => "test bio" } },
+        { :return_url => stories(:one) }
+    end
+    assert_redirected_to stories(:one)
+  end
+  
+  test "should show user" do
+    get :show, { :id => "charlie" }
+    assert_response :success
+    assert_select ".content_box.larger h2", "charlie's posts"
+    assert_select ".user_posts .linkbox"
+    assert_select ".user_posts .comment"
+    assert_equal users(:charlie).points, css_select(".info_points .total")[0].children[0].content.split(" ")[0].to_i
+  end
 end
